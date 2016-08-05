0e09e677e1242eb13b06af02a26116e28e4ec272
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2016-08-05 09:18:55 -0500

    Added about page and index

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,30 @@
-console.log('This would be the main JS file.');
+
+$(document).ready(function(){
+   
+    $('.About').on("click", function(){
+         window.location.href='About.html';
+    });
+    
+    $('.Videos').on("click", function(){
+         window.location.href='Videos.html';
+    });
+    
+    $('.Theory').on("click", function(){
+         window.location.href='Theory.html';
+    });
+    
+    $('.Examples').on("click", function(){
+         window.location.href='Examples.html';
+    });
+    
+    $('.Start').on("click", function(){
+         window.location.href='Start.html';
+    });
+
+    $('.Install').on("click", function(){
+         window.location.href='Install.html';
+    });
+    
+});
+
+
